81c3107253d068b1e5d5a6d70968a949ba6e2d28
Luis Octavio Ramirez  Cruz <luis_ram1997@hotmail.com>
2020-05-13 17:26:30 -0500

    reorder the flow of the program and delete unnecessary lines

--- bin/main.rb
+++ bin/main.rb
@@ -1,20 +1,16 @@
 #!/usr/bin/env ruby
 
-# libraries
-
-# add link to the page which is going to be scraped
-
 puts 'This program shows you the last 6 patches of the League of Legends video game'
 puts 'You can choose one of them to see the stats changes of champions and items in that patch'
 
 # print versions patches with a number in the right side as a reference
 
-# validate if the choice is valid
-
 puts 'Select the version patch where are the changes that you want to see'
 puts 'To choose you need to write the number on the left side of the patch you want to see'
 
 # add the choice of the user to a variable
+
+# validate if the choice is valid
 
 # get the link of the patch page that the user chose
 
