3246401a9128bacac5ca0cf010804f9384376082
Luis Octavio <luis_ram1997@hotmail.com>
2020-05-14 15:56:33 -0500

    add a tests for the yes_no_answer? method

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -1,18 +1,33 @@
-require "./lib/validation.rb"
+require './lib/validation.rb'
 describe Validation do
-  describe "#valid_choice?" do
-    let(:validation) { Validation.new }
-    it "the input is text" do
+  let(:validation) { Validation.new }
+  describe '#valid_choice?' do
+    it 'the input is text' do
       validation.user_input = 'g'.to_i
       expect(validation.valid_choice?).to eql(false)
     end
-    it "the input is not a number between 1 and 6" do
+    it 'the input is not a number between 1 and 6' do
       validation.user_input = 8
       expect(validation.valid_choice?).to eql(false)
     end
-    it "the input is a number between 1 and 6" do
+    it 'the input is a number between 1 and 6' do
       validation.user_input = 2
       expect(validation.valid_choice?).to eql(true)
+    end
+  end
+
+  describe '#yes_no_answer?' do
+    it 'the input is neither yes nor no' do
+      validation.user_input = 'has'
+      expect(validation.yes_no_answer?).to eql(false)
+    end
+    it 'the input is yes' do
+      validation.user_input = 'no'
+      expect(validation.yes_no_answer?).to eql(true)
+    end
+    it 'the input is no' do
+      validation.user_input = 'yes'
+      expect(validation.yes_no_answer?).to eql(true)
     end
   end
 end
